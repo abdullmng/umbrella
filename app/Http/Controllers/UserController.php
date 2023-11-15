56f9e5b9cbccab354cee66a1ff113381e175050c
Muhammad Abdullahi <abdullmng@gmail.com>
2023-11-15 12:38:19 +0100

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -90,6 +90,7 @@ class UserController extends Controller
         $course = Course::find($course_id);
         $code  = $request->coupon;
         $user = auth()->user();
+        $config = app('settings');
 
         $coupon = $this->getCoupon($code);
         if (!is_null($coupon) && $coupon->status == 'unused')
@@ -106,6 +107,69 @@ class UserController extends Controller
                     'status' => 'active',
                     'date_activated' => date('Y-m-d'),
                 ]);
+
+                if ($course->id == $config['registration_course'])
+                {
+                    $ref = User::where('id', $user->ref_id)->first();
+                    if ($ref) {
+                        $ref_com = $this->percentage($course->amount, $config['referral_commission']);
+                        if ($config['referral_type'] == 'fixed')
+                        {
+                            $ref_com = $config['referral_commission'];
+                        }
+                        Earning::updateOrCreate([
+                            'user_id' => $ref->id,
+                            'course_id' => $user->id,
+                            'amount' => $ref_com,
+                            'type' => 'referral_commission',
+                        ],[
+                            'user_id' => $ref->id,
+                            'course_id' => $user->id,
+                            'amount' => $ref_com,
+                            'type' => 'referral_commission',
+                            'day' => date('Y-m-d'),
+                        ]);
+
+                        $upper_ref = $ref->ref_id;
+                        if (!is_null($upper_ref))
+                        {
+                            $sub_ref_com = $this->percentage($course->amount, $config['sub_referral_commission']);
+                            if ($config['referral_type'] == 'fixed')
+                            {
+                                $sub_ref_com = $config['sub_referral_commission'];
+                            }
+                            Earning::updateOrCreate([
+                                'user_id' => $upper_ref,
+                                'course_id' => $ref->id,
+                                'amount' => $sub_ref_com,
+                                'type' => 'referral_commission',
+                            ],[
+                                'user_id' => $upper_ref,
+                                'course_id' => $ref->id,
+                                'amount' => $sub_ref_com,
+                                'type' => 'referral_commission',
+                                'day' => date('Y-m-d'),
+                            ]);
+                        }
+                    }
+
+                    if ($config['allow_cashback'] == 'true')
+                    {
+                        $cashback = $this->percentage($course->amount, $config['cashback_amount']);
+                        if ($config['cashback_type'] == "fixed")
+                        {
+                            $cashback = $config['cashback_amount'];
+                        }
+                        Earning::updateOrCreate(['user_id' => $user->id, "course_id" => $course->id],[
+                            'user_id' => $user->id,
+                            'course_id' => $course->id,
+                            'amount' => $cashback,
+                            "type" => 'task_commission',
+                            "day" => date("Y-m-d")
+                        ]);
+                    }
+                }
+
                 return back()->with('success', 'course activated');
             }
             return back()->withErrors(['error' => 'This coupon cannot be used for this activation!']);
